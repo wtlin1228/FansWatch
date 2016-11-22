8779a3a6d92a5446bc836fca3184310e1c7fce06
wtlin1228 <wtlin0711@gmail.com>
2016-11-21 23:16:12 -0800

    add comments in fb_api.rb to make it easier understand what data we can get

--- lib/fanswatch/fb_api.rb
+++ lib/fanswatch/fb_api.rb
@@ -27,20 +27,67 @@ module FansWatch
       JSON.load(response.to_s)
     end
 
+    # Get the fans page's name and id
+    #   ex: @id="159425621565",
+    #       @name="Inside 硬塞的網路趨勢觀察"
     def page_info(page_id)
       fb_resource(page_id)
     end
 
-    def posting(posting_id) 
-      fb_resource(posting_id) 
-    end
-
+    # Get the collection of fans page's postings' (message, created_time and id)
+    #   ex : {
+    #           "message"=> "日前金管會確認 Apple Pay、Samsung Pay 等行動支付服務將可進入台灣市場運作後，Google 所提出 Android Pay 也確認將可進入台灣市場。而從相關說法透露，Android Pay 最快將會在近期內於台灣市場推行，但具體時間依然要等 Google 說明。",
+    #           "created_time"=>"2016-11-22T05:58:05+0000",
+    #           "id"=>"159425621565_10153840498126566"
+    #         },
+    #
+    #                              .
+    #                              .
+    #                              .
+    # 
+    #        {
+    #           "message"=>"橘子集團發表新的揪團通訊 App：  BeanGo！ 記者會",
+    #           "created_time"=>"2016-11-22T05:38:12+0000",
+    #           "id"=>"159425621565_10153840469861566"
+    #         }
     def page_feed(page_id)
       feed_response = 
       	HTTP.get(URI.join(fb_resource_url(page_id), 'feed'),
                  params: { access_token: @access_token })
       JSON.load(feed_response.to_s)['data']
     end
+
+    # Get the single posting's basic information by posting id 
+    #   which we can get from page_feed
+    #   ex : {
+    #           "created_time"=>"2016-11-22T03:44:08+0000",
+    #           "message"=>"網路中立性原則要求網路服務供應商及政府應平等處理所有網路上的資料，不差別對待或依不同用戶、內容、網站、平台、應用、接取裝置類型或通訊模式而差別收費。而這也是早逝的 RSS 共同開發者與 Reddit 創辦人 Aaron Swartz 長期以來所不斷捍衛的信念和目標。",
+    #           "id"=>"159425621565_10153840361851566"
+    #         }
+    def posting(posting_id) 
+      fb_resource(posting_id) 
+    end
+
+    # Get the attachment of a posting
+    #   ex : {
+    #           "description"=>"美國下一任總統川普正式任命兩名顧問，以幫助他完成在聯邦通信委員會監督（FCC）的過渡。這兩名顧問分別是 Jeff Eisenach 和 Mark Jamison，均是網路中立性原則的激烈反對者。",
+    #           "media"=>
+    #            {
+    #               "image"=>
+    #                {
+    #                  "height"=>720,
+    #                  "src"=>"https://external.xx.fbcdn.net/safe_image.php?d=AQBO6Ai9foDoVI1f&w=720&h=720&url=https%3A%2F%2Fwww.inside.com.tw%2Fwp-content%2Fuploads%2F2016%2F11%2F24343769071_d409f67726_k.jpg&cfs=1&sx=450&sy=0&sw=1365&sh=1365",
+    #                  "width"=>720
+    #                }
+    #            },
+    #           "target"=>
+    #            {
+    #               "url"=>"https://www.facebook.com/l.php?u=https%3A%2F%2Fwww.inside.com.tw%2F2016%2F11%2F22%2Fdonald-j-trump-net-neutrality&h=ATO8VyRbirwfQAQraYgt8e1aQEoG6oAneIqomLzhe0gGVg_0iE5TAeJhKyhjRzwuCtvKy2mDXL6iSKtwmB6ABCLbyE8&s=1&enc=AZMg3ju-UJWf_VvBESGjeaIUYH7vIVJLQaULvMxBrH0BI7tKrX3KXKyvL-oU5dMdUzANLjxddUlsQOX7Auz-sChD"
+    #            },
+    #            "title"=>"川普的 FCC 團隊，可能會終結網路中立性原則",
+    #            "type"=>"share",
+    #            "url"=>"https://www.facebook.com/l.php?u=https%3A%2F%2Fwww.inside.com.tw%2F2016%2F11%2F22%2Fdonald-j-trump-net-neutrality&h=ATO8VyRbirwfQAQraYgt8e1aQEoG6oAneIqomLzhe0gGVg_0iE5TAeJhKyhjRzwuCtvKy2mDXL6iSKtwmB6ABCLbyE8&s=1&enc=AZMg3ju-UJWf_VvBESGjeaIUYH7vIVJLQaULvMxBrH0BI7tKrX3KXKyvL-oU5dMdUzANLjxddUlsQOX7Auz-sChD"
+    #         }
 
     def posting_attachments(posting_id)
       attachments_response = 
